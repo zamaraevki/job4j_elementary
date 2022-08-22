1ec35a0dae411f3464548504a6653f35d1018a21
Константин <zamaraevki@gmai.com>
2022-08-22 14:23:18 +0500

    4.1. Разбор первой программы. [#159033 #102300]

---
+++ src/main/java/ru/job4j/StudentInfo.java
@@ -0,0 +1,8 @@
+package ru.job4j;
+
+public class StudentInfo {
+    public static void main(String[] args) {
+        System.out.println("Zamaraev Konstantin");
+        System.out.println("22.08.2022");
+    }
+}
