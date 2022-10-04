5856113008bb7c471ca16d516fb3b5861b389a28
Константин <zamaraevki@gmai.com>
2022-10-04 18:02:52 +0500

    6.5.2. Поиск минимума в диапазоне. [#179804 #102275]

--- src/test/java/ru/job4j/array/FindLoopTest.java
+++ src/test/java/ru/job4j/array/FindLoopTest.java
@@ -21,4 +21,37 @@ public class FindLoopTest {
         int expected = -1;
         assertThat(result).isEqualTo(expected);
     }
+
+    @Test
+    public void whenDiapasonHas2Then3() {
+        int[] data = new int[] {5, 2, 10, 2, 4};
+        int el = 2;
+        int start = 2;
+        int finish = 4;
+        int result = FindLoop.indexInRange(data, el, start, finish);
+        int expected = 3;
+        assertThat(result).isEqualTo(expected);
+    }
+
+    @Test
+    public void whenDiapasonHas3Then7() {
+        int[] data = new int[] {1, 2, 3, 4, 5, 6, 7, 8};
+        int el = 6;
+        int start = 3;
+        int finish = 7;
+        int result = FindLoop.indexInRange(data, el, start, finish);
+        int expected = 5;
+        assertThat(result).isEqualTo(expected);
+    }
+
+    @Test
+    public void whenDiapasonHasNot3Then7() {
+        int[] data = new int[] {1, 2, 3, 4, 5, 6, 7, 8};
+        int el = 9;
+        int start = 3;
+        int finish = 7;
+        int result = FindLoop.indexInRange(data, el, start, finish);
+        int expected = -1;
+        assertThat(result).isEqualTo(expected);
+    }
 }
